c3f2697894c254e61a66e73cd8faabce8b338561
shivang085 <shivang.ch01@gmail.com>
2026-08-19 22:42:55 +0530

    Create Payment table for transaction records

--- SCHEMA/Payment.sql
+++ SCHEMA/Payment.sql
@@ -1,12 +1,21 @@
-CREATE TABLE `Show`(
-    ShowID INT PRIMARY KEY AUTO_INCREMENT,
-    ShowDate DATE NOT NULL,
-    ShowTime TIME NOT NULL,
-    TicketPrice INT NOT NULL,
-    AvailableSeats INT NOT NULL,
-    ShowStatus ENUM('SCHEDULED', 'ONGOING', 'COMPLETED', 'CANCELLED') NOT NULL DEFAULT 'SCHEDULED',
-    MovieID INT NOT NULL,
-    ScreenID INT NOT NULL,
-    FOREIGN KEY (MovieID) REFERENCES Movie(MovieID),
-    FOREIGN KEY (ScreenID) REFERENCES Screen(ScreenID)
-);
+CREATE TABLE Payment(
+    PaymentID INT PRIMARY KEY AUTO_INCREMENT,
+    PaymentMethod ENUM(
+    'CREDIT_CARD',
+    'DEBIT_CARD',
+    'UPI'
+    ) NOT NULL,
+    TransactionID VARCHAR(100) UNIQUE NOT NULL,
+    PaymentAmount BIGINT NOT NULL,
+    PaymentDateTime TIMESTAMP NOT NULL,
+    PaymentStatus ENUM(
+        'PENDING',
+        'SUCCESS',
+        'FAILED',
+        'PROCESSING'
+    ) NOT NULL  DEFAULT 'PENDING',
+    BookingID INT NOT NULL,
+
+    FOREIGN KEY (BookingID)
+        REFERENCES Booking(BookingID)
+)
